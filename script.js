78729d5b05ba8a4d6f139b40a35dc920dab6a1c3
Kevin Ngo <ktn9699@gmail.com>
2023-12-29 03:06:02 -0500

    fixed bug with chained operations

--- script.js
+++ script.js
@@ -5,113 +5,111 @@ let operateKeys = document.querySelectorAll('.symbol');
 let equal = document.querySelector('#equal')
 let clear = document.querySelector('#AC')
 let negative = document.querySelector('#plusminus')
-let operator = '';
+let backspace = document.querySelector('#backspace')
 let decimal = document.querySelector('#decimal')
-let current = '';
-let previous = '';
-let result;
-
-// By using the Array.forEach() method, we go through all of the classes for the 
-// number keys and add an eventListener to all of them. When a number is clicked, 
-// it will be added to the ongoing display total. The display totaled is then converted
-// into a float for uses of operation. 
+let operationSymbol = '';
+let firstNum = ''
+let secondNum = '';
+let tempHolder = '';
+let result = 0;
+let total = ''
 
 numKeys.forEach((element) => element.addEventListener('click', () => {
-if (current === '0') {
-    current = '';
-}
-    current += element.textContent;
-    displayScreen.innerHTML = current;
-}))
-
-// Getting the symbol that is clicked by going through all of the symbols. 
-// When a symbol is clicked, the input will be inside of 'currentKey'. 
-
-operateKeys.forEach((element) => element.addEventListener('click', () => {
-    operator = element.textContent;
-    previous = current;
-    current = '';
-if (operator.textContent !== '') {
-    previouScreen.textContent = previous + ' ' + operator;
-    displayScreen.textContent = previous;
-}
-  
-}))
-
-
-function operate(current,previous,operator) {
-   
-    if (operator === '+'){
-    result = add(parseFloat(current),parseFloat(previous));
+    if (tempHolder === '0') {
+        tempHolder = ''; 
     }
-    else if (operator === '-') {
-      result = subtract(parseFloat(current),parseFloat(previous));
-    }
-    else if (operator === '/') {
-        result = divide(parseFloat(current),parseFloat(previous));
-      }
-      else if (operator === '*') {
-        result = multiply(parseFloat(current),parseFloat(previous));
-      }
-      else if (operator === '%') {
-        result = modulus(parseFloat(current),parseFloat(previous));
-      }
-     
-    return result;
-    }
+       tempHolder += element.textContent;
+        displayScreen.textContent = tempHolder;
+    }))
 
-    equal.addEventListener('click', () => {
-        operate(current,previous,operator);
-        displayScreen.textContent = result;
-        current = result;
-        previouScreen.textContent = current;
-      
-    }
-        )
-
-    clear.addEventListener('click', () => {
-        current = '';
-        previous = ''
-        result = '0'
-            displayScreen.textContent = result;
-            current = result;
-            previouScreen.textContent = '';
-          
+    operateKeys.forEach((element) => element.addEventListener('click', () => { 
+        if (firstNum == '') {
+            firstNum = tempHolder;
+            tempHolder = '';
         }
-            )
+if (firstNum !== '' && operationSymbol !== '') {
+    secondNum = tempHolder;
 
-            function dot() {
-              if (!current.includes(".")) {
-                  current += '.';
-              }
-              
-              }
-              
-              negative.addEventListener('click', () => {
-                  if (current !== '') {
-                      current = parseFloat(current) * -1;
-                      displayScreen.textContent = current;
-                  }
-              });
-              
+}
+
+if (firstNum !== '' && operationSymbol !== '' && secondNum !== ''){
+    displayScreen.textContent= '0';
+    operate(firstNum,secondNum, operationSymbol)
+    firstNum = total;   
+    secondNum = '';
+    tempHolder = '';
+}       
+
+if (firstNum == '' && operationSymbol !== '' && secondNum !== ''){
+    firstNum = tempHolder;
+}       
+operationSymbol = element.textContent;
+    previouScreen.textContent = firstNum+ " " + operationSymbol;   
+
+if (firstNum == '0') {
+        previouScreen.textContent = '';
+    }
+}))
+    
+            clear.addEventListener('click', () => {
+                firstNum = '';
+                secondNum = ''
+                tempHolder = '';
+                total = '0';
+                    displayScreen.textContent = total;
+                    firstNum = total;
+                    previouScreen.textContent = '';})
+ 
               decimal.addEventListener('click', () => {
-              dot()
-              })
+                if (!tempHolder.includes(".")) {
+                    tempHolder += '.'; }
+            })
+        
+            negative.addEventListener('click', () => {
+                if (tempHolder !== '') {
+                    tempHolder = parseFloat(tempHolder) * -1;
+                    displayScreen.textContent = tempHolder;
+                }});
 
-function add(a,b) {
-    return b+a;
-}
-function subtract(a,b) {
-    return b-a;
-}
-function divide(a,b) {
-    return b/a
-}
-function multiply(a,b) {
-    return b*a;
-}
-function modulus(a,b) {
-    return b%a;
-}
+            equal.addEventListener('click', () => {
+                if (tempHolder != '' && firstNum !== '') {
+                    secondNum = tempHolder;
+                    operate(firstNum, secondNum, operationSymbol);
+                } 
+            } )
+            
+            backspace.addEventListener('click', () => {
+            if (secondNum == '' && firstNum !== '') {
+                tempHolder = tempHolder.slice(0, -1);
+                displayScreen.textContent = tempHolder;
+            }
+           
+             if (total !== '') {
+                total = total.slice(0,-1);
+                displayScreen.textContent = total;
+             }
+            } )
+      
+            function operate(firstNum,secondNum, operationSymbol){
+                firstNum = parseFloat(firstNum);
+                secondNum = parseFloat(secondNum);
+            
+                switch(operationSymbol) {
+                    case '+': 
+                    result = firstNum + secondNum;
+                    break;
+                    case '-':
+                    result = firstNum - secondNum;
+                    break;
+                    case '*':
+                    result = firstNum * secondNum;
+                    break;
 
-
+                    case '/':
+                    result = firstNum / secondNum;
+                        break;
+                    }
+                    total = result.toString()
+                    displayScreen.textContent =  total;
+                  
+            }
